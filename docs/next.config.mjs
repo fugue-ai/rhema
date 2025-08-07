7154016acd34c96365401b95f200634abb5a26ea
goedelsoup <goedelsoup@gmail.com>
2025-08-06 21:42:47 -0400

    chore: fixes for nextra 4

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,14 +1,16 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
+  latex: true,
+  search: {
+    codeblocks: false
+  },
   contentDirBasePath: process.env.NODE_ENV === 'production' ? '/rhema' : '/',
 })
 
 export default withNextra({
-  turbopack: {
-    resolveAlias: {
-      // Path to your `mdx-components` file with extension
-      'next-mdx-import-source-file': './src/mdx-components.tsx'
-    }
-  }
+  output: 'export',
+  experimental: {
+    optimizePackageImports: false,
+  },
 })
